3fcaacee1aacd4a804339f4418cc132d7c275604
mdeodhar@hotmail.com <mdeodhar@hotmail.com>
2020-05-20 08:42:18 +0530

    Added MVC Controllers using ervices.AddControllersWithViews();

---
+++ Proj_3_Github_Test/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proj_3_Github_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to Controller";
+        }
+    }
+}
--- Proj_3_Github_Test/Startup.cs
+++ Proj_3_Github_Test/Startup.cs
@@ -16,6 +16,7 @@ namespace Proj_3_Github_Test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,10 +46,12 @@ namespace Proj_3_Github_Test
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
 
             //app.UseEndpoints(endpoints =>
